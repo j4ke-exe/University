f50a769ee8f24842e1a03fb234b119b7cb05acc3
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2022-12-24 18:05:03 -0700

    Update Feet_to_inches.py

--- Python/Feet_to_inches.py
+++ Python/Feet_to_inches.py
@@ -5,8 +5,6 @@
 
 def print_total_inches(num_feet, num_inches):
     print("Total inches:", num_feet * 12 + num_inches)
-    # Alternate print method using f
-    # print(f'Total inches: {num_feet} * 12 + {num_inches}')
 
 feet = int(input())
 inches = int(input())
