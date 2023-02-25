31ca7dd4d2d8fa235706bac8e3a539c1c34b27e9
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2023-02-25 16:07:08 -0700

    Create assiginingSum.java

---
+++ Java/assiginingSum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class AssigningSum {
+   public static void main(String[] args) {
+      int numCoins;
+      int numNickels;
+      int numDimes;
+
+      numNickels = 5;
+      numDimes = 6;
+
+      numCoins = numNickels + numDimes;
+
+      System.out.print("There are ");
+      System.out.print(numCoins);
+      System.out.println(" coins");
+   }
+}
