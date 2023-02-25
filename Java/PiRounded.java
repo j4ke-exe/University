eb67df824f28adb3a4137e0d8960b13740981893
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2023-02-25 16:43:14 -0700

    Create PiRounded.java

---
+++ Java/PiRounded.java
@@ -0,0 +1,6 @@
+System.out.println("Default output of Pi: " + Math.PI);
+System.out.print("Pi reduced to 4 digits after the decimal: ");
+System.out.printf("%.4f", Math.PI);
+
+// Pi reduced to 4 digits after the decimal: 3.1416
+// Default output of Pi: 3.141592653589793
