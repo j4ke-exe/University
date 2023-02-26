a9aa27c1d737e74e4da97bd2ffd19e6c7457e611
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2023-02-25 17:45:10 -0700

    Create stringVariables.java

---
+++ Java/stringVariables.java
@@ -0,0 +1,17 @@
+public class SentenceFromStrings {
+   public static void main(String[] args) {
+      String sentenceSubject;
+      String sentenceVerb;
+      String sentenceObject = "an apple";
+
+      sentenceSubject = "boy";
+      sentenceVerb = "ate";
+
+      System.out.print("A ");
+      System.out.print(sentenceSubject + " ");
+      System.out.print(sentenceVerb + " ");
+      System.out.println(sentenceObject + ".");
+   }
+}
+
+// Output: A boy ate an apple.
