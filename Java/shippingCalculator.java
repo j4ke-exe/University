acd05b37eca2b36d532961078bab531703899368
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2023-02-25 17:03:39 -0700

    Create shippingCalculator.java

---
+++ Java/shippingCalculator.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ShippingCalculator {
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      int shipWeightPounds;
+      int shipCostCents = 0;
+      final int FLAT_FEE_CENTS = 75;
+
+      final int CENTS_PER_POUND = 25;
+      shipWeightPounds = scnr.nextInt();
+      shipCostCents = FLAT_FEE_CENTS + (CENTS_PER_POUND * shipWeightPounds);
+
+      System.out.println("Weight(lb): " + shipWeightPounds);
+      System.out.println("Flat fee(cents): " + FLAT_FEE_CENTS);
+      System.out.println("Cents per pound: " + CENTS_PER_POUND);
+      System.out.println("Shipping cost(cents): " + shipCostCents);
+   }
+}
+
+// Testing shipWeightPounds = 10
+// Output: Weight(lb): 10
+//         Flat fee(cents): 75
+//         Cents per pound: 25
+//         Shipping cost(cents): 325
