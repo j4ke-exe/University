3811af73b71a2c07cdba88db03433f3fd1e32d6b
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2023-03-08 16:33:36 -0700

    Update nestedLoops.java

--- Java/nestedLoops.java
+++ Java/nestedLoops.java
@@ -1,3 +1,12 @@
+/*
+Question: Print numbers 0, 1, 2, ..., userNum as shown, with each number indented by that number of spaces. For each printed line, print the leading spaces, then the number, and then a newline. Hint: Use i and j as loop variables (initialize i and j explicitly). Note: Avoid any other spaces like spaces after the printed number. Ex: userNum = 3 prints:
+0
+ 1
+  2
+   3
+*/
+
+// Solution
 import java.util.Scanner;
 public class NestedLoop {
    public static void main (String [] args) {
