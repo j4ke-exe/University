77f65b3cd090c29099ac1203b4a22e6464490f22
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2023-04-21 23:36:14 -0600

    Create builtInReversedSorting.py

---
+++ Python/Sorting/builtInReversedSorting.py
@@ -0,0 +1,16 @@
+num_list = [3, 7, 2, 8, 12, 4, 9, 5]
+sorted_num_list = sorted(num_list, reverse = True)
+print('UNSORTED:', num_list)
+print('REVERSE SORTED:', sorted_num_list)
+print()
+
+fruit_list = ["grape", "banana", "apple", "strawberry", "blueberry"]
+print('UNSORTED:', fruit_list)
+fruit_list.sort(reverse = True)
+print('REVERSE SORTED:', fruit_list)
+ 
+# UNSORTED: [3, 7, 2, 8, 12, 4, 9, 5]
+# REVERSE SORTED: [12, 9, 8, 7, 5, 4, 3, 2]
+
+# UNSORTED: ['grape', 'banana', 'apple', 'strawberry', 'blueberry']
+# REVERSE SORTED: ['strawberry', 'grape', 'blueberry', 'banana', 'apple']
