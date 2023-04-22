92116a49f8595451f73a0523f03d2819c8aa5181
Jake Garrison <101604552+wayahlife@users.noreply.github.com>
2023-04-21 23:35:25 -0600

    Create builtInSorting.py

---
+++ Python/Sorting/builtInSorting.py
@@ -0,0 +1,18 @@
+# Using sorted() function
+num_list = [3, 7, 2, 8, 12, 4, 9, 5]
+sorted_num_list = sorted(num_list)
+print('UNSORTED:', num_list)
+print('SORTED:', sorted_num_list)
+print()
+
+# Using sort() method
+fruit_list = ["grape", "banana", "apple", "strawberry", "blueberry"]
+print('UNSORTED:', fruit_list)
+fruit_list.sort()
+print('SORTED:', fruit_list)
+ 
+# UNSORTED: [3, 7, 2, 8, 12, 4, 9, 5]
+# SORTED: [2, 3, 4, 5, 7, 8, 9, 12]
+
+# UNSORTED: ['grape', 'banana', 'apple', 'strawberry', 'blueberry']
+# SORTED: ['apple', 'banana', 'blueberry', 'grape', 'strawberry']
